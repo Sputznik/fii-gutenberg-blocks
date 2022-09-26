54a5caec9d286f6ab6843ef17fdaf6322fca170f
psychome004 <anil.stephen66@gmail.com>
2022-09-26 16:33:55 +0530

    Build release

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '65f88af024f9a42096ee');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => 'a7f025d6cd713088ac25');
