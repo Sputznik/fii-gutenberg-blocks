f3ca4e1255829579d70ccbb1451b777469a33e4c
psychome004 <anil.stephen66@gmail.com>
2023-05-02 17:19:22 +0530

    Add customizer data in block templates

--- render-callback/editors-note.php
+++ render-callback/editors-note.php
@@ -1,3 +1,4 @@
+<?php global $fii_customize; ?>
 <div <?php echo get_block_wrapper_attributes( array('class'=> 'fii-dec-bf fii-dec-af') ); ?>>
-  <span class="title">» Editor’s Note: </span><?php echo $atts['note']; ?>
+  <span class="title">» <?php echo $fii_customize->get_theme_option( 'translation', 'editors_note', 'Editors Note' ); ?>: </span><?php echo $atts['note']; ?>
 </div>
--- render-callback/fii-also-read.php
+++ render-callback/fii-also-read.php
@@ -1,4 +1,5 @@
+<?php global $fii_customize; ?>
 <div <?php echo get_block_wrapper_attributes( array('class'=> 'fii-read fii-dec-bf fii-dec-af') ); ?>>
-  <span class="title">» Also read: </span>
+  <span class="title">» <?php echo $fii_customize->get_theme_option( 'translation', 'also_read', 'Also read' ); ?>: </span>
   <a href="<?php _e( $atts['url'] );?>"><?php _e( $atts['text'] );?></a>
 </div>
